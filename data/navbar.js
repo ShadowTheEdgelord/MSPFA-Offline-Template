e33bac9414d37cfd0c095a708b6a2460f174e62c
ShadowTheEdgelord <54421333+ShadowTheEdgelord@users.noreply.github.com>
2021-08-07 11:36:03 +1000

    1.0
    Initial

---
+++ data/navbar.js
@@ -0,0 +1,16 @@
+//This allows you to navigate to different pages within the browser. If you want to edit it, all you have to do is change the links and names as well as the colour and it will update across all pages that have this javascript loaded.
+//important for css formatting
+document.write("<a href='Title Page.html' style='color: #29ff4a;'>Home</a>");
+document.write("<span style='color: #fff;'> | </span>");
+//A simple separator
+document.write("<a href='https://mspfa.com/' target='_blank' style='color: #29ff4a;'>MSPFA</a>");
+//target='_blank' will open the page in a new tab
+document.write("<img src='img/seperator.gif'>");
+//This is the little candycorn image
+document.write("<a href='Extras.html' style='color: #39d5f6;'>Extras</a>");
+document.write("<span style='color: #fff;'> | </span>");
+document.write("<a href='Credits.html' style='color: #39d5f6;'>Credits</a>");
+document.write("<img src='img/seperator.gif'>");
+document.write("<a href='Log.html' style='color: #ffb529;'>Log</a>");
+document.write("<span style='color: #fff;'> | </span>");
+document.write("<a href='Map.html' style='color: #ffb529;'>Map</a>");
